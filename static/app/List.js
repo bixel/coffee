40476b1a0d6421e0979626bc42f897c998b3c8f1
Kevin Heinicke <kevin@kehei.de>
2016-11-27 18:44:24 +0100

    Prepare for iterating users

--- static/app/List.js
+++ static/app/List.js
@@ -9,20 +9,30 @@ export default class List extends Component {
         "Kaffee",
         "Milchkaffee",
       ],
+      users: [],
     };
   }
 
   render(){
+    const rows = this.state.users.map((user, i) => {
+      const background = (i + 1) % 2 ? "#E1EBCE" : "";
+      return <Row
+        products={this.state.products}
+        name={user}
+        consume="5"
+        style={{background: background, padding: "4px"}}
+      />
+    })
     return <div className="container">
       <div className="row"><div className="col-xs-12">
         <h1>Kaffeeliste</h1>
       </div></div>
       <div className="row">
-      <div className="col-xs-3"><h4>Name</h4></div>
-      <div className="col-xs-3"><h4>Verbrauch Heute</h4></div>
-      <div className="col-xs-6"></div>
+        <div className="col-xs-3"><h4>Name</h4></div>
+        <div className="col-xs-3"><h4>Kaffee Heute</h4></div>
+        <div className="col-xs-6"></div>
       </div>
-      <Row products={this.state.products} name="Kevin Heinicke" consume="5" style={{background: "#E1EBCE", padding: "4px"}} />
+      {rows}
     </div>
   }
 }
