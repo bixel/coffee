1da6420493c372326e7c8d3aca5098e58eedc5a7
VukanJ <vukan.jevtic@tu-dortmund.de>
2020-01-06 21:48:36 +0100

    Updated start-dev.sh accordingly

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,12 @@
-docker start coffee-db || docker run -d --name coffee-db -p 27017:27017 mongo
-docker start coffee-ldap || docker run --name coffee-ldap -d -p 389:389 -e SLAPD_PASSWORD=password -e SLAPD_DOMAIN=coffee.ldap dinkel/openldap
-ldapadd -x -D cn=admin,dc=coffee,dc=ldap -w password -f ./testing/ldap_test_data.ldif
-npm start &
-COFFEE_DEBUG=True FLASK_ENV=development FLASK_APP=coffee.py flask run &
+set -e
+if [ $(id -u) -ne 0 ]
+    then echo "docker needs sudo priviledges. Please run $0 as root"
+    exit
+else
+    docker start coffee-db || docker run -d --name coffee-db -p 27017:27017 mongo
+    docker start coffee-ldap || docker run --name coffee-ldap -d -p 389:389 -e SLAPD_PASSWORD=password -e SLAPD_DOMAIN=coffee.ldap dinkel/openldap
+    ldapadd -x -D cn=admin,dc=coffee,dc=ldap -c -w password -f ./testing/ldap_test_data.ldif || true
+    npm start &
+    export SECRET_KEY=SECRET
+    COFFEE_DEBUG=True FLASK_ENV=development FLASK_APP=coffee.py flask run &
+fi
